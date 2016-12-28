9e41d4ba8bea188fe13b0b465848865846b9eefd
Mathieu PATUREL <australie.p@gmail.com>
2016-12-28 18:59:51 +1100

    auto reload plugin on save

--- BT.py
+++ BT.py
@@ -1,1 +1,12 @@
+import sublime_plugin
 from .utils import *
+
+class BoxyDevListner(sublime_plugin.EventListener):
+
+    def on_post_save(self, view):
+        if os.path.dirname(__file__) not in view.file_name():
+            return
+        sublime.run_command('reload_plugin', {
+            'main': '$packages/Boxy Theme/BT.py',
+            'folders': ['utils']
+        })
